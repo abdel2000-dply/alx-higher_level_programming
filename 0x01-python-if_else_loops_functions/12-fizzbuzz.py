8339c53161c5a22fd068e7e6501036b19d93fb5e
abdel2000-dply <abno870@gmail.com>
2023-06-07 23:28:52 +0100

    task 12 :fire:

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for n in rang(1, 101):
+    for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
         elif n % 3 == 0:
