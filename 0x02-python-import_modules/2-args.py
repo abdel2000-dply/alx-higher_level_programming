0c66ecc961d9815405afcd63276f11757ac08f5c
abdel2000-dply <abno870@gmail.com>
2023-06-08 20:38:30 +0100

    task 2 :fire:

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 if __name__ == "__main__":
     ar = sys.argv[1:]
-    ar_len = len(ar)
+    arl = len(ar)
 
-    if ar_len == 0:
+    if arl == 0:
         print("0 arguments.")
     else:
-        print("{} {}:".format(ar_len, "argument" if ar_len == 1 else "arguments"))
-        for i in range(ar_len):
+        print("{} {}:".format(arl, "argument" if arl == 1 else "arguments"))
+        for i in range(arl):
             print("{}: {}".format(i + 1, ar[i]))
