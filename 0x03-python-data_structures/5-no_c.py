89be6b015e2e996c3266bb90f90e19d24096d5aa
abdel2000-dply <abno870@gmail.com>
2023-06-11 01:44:02 +0100

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     if my_string:
         new = ""
-	for c in my_string
-            if 'c' in my_string or 'C' in my_string
+	for c in my_string:
+            if 'c' in my_string or 'C' in my_string:
                 continue
             new = my_string
     return new
