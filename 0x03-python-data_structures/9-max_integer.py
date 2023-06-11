7f5bcbf83750a122616cd462ca25074dcd187391
abdel2000-dply <abno870@gmail.com>
2023-06-11 22:11:09 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    maxnum = 0
+    for i in my_list:
+        if i > maxnum:
+        maxnum = i
+    return maxnum
