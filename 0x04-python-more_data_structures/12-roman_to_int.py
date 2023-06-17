c3602b4e45375704e17f0266fe5f309575252015
abdel2000-dply <abno870@gmail.com>
2023-06-17 16:14:21 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,14 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
 
-    val = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     res = 0
-    l = len(roman_string)
-    for i in range(l):
+    leng = len(roman_string)
+    for i in range(leng):
         if roman_string[i] not in val:
             return 0
-        if i < l - 1 and val[roman_string[i]] < val[roman_string[i + 1]]:
+        if i < leng - 1 and val[roman_string[i]] < val[roman_string[i + 1]]:
             res -= val[roman_string[i]]
         else:
             res += val[roman_string[i]]
