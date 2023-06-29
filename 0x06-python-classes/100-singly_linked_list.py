dfbf50bacac30d435fdf33eb8f1fba6812f760c0
abdel2000-dply <abno870@gmail.com>
2023-06-29 19:13:40 +0100

    task 100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This is a Node"""
+
+
+class Node:
+    """class Node"""
+
+    def __init__(self, data, next_node=None):
+        """init a Node"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """get data"""
+        return self.__data
+
+    @data.setter
+    def data(self, n):
+        """set data"""
+        if type(n) is not int:
+            raise TypeError('data must be an integer')
+        self.__data = n
+
+    @proprty
+    def next_node(self):
+        """get next node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, n):
+        """Set next_node"""
+        if type(n) is not Node and n is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = n
+
+    class SinglyLinkedList:
+        """Sinlgy Linked List"""
+        def __init__(self):
+            """init empty list"""
+            self.head = None
+
+        def sorted_insert(self, value):
+            """insert a node"""
+            new_node = Node(value)
+            if self.head is None:
+                self.head = new_node
+            elif value <= self.head.data:
+                new_node.next_node = self.head
+                self.head = new_node
+            else:
+                curr = self.head
+                while curr.next_node is not None \
+                        and value > curr.next_node.data:
+                    curr = curr.next_node
+                new_node.next_node = curr.next_node
+                curr.next_node = new_node
+
+        def __str__(self):
+            """str representation"""
+            curr = self.head
+            str = ""
+            while curr is not None:
+                str += str(curr.data) + "\n"
+                curr = curr.next_node
+            return str
