b3ca9925388d9cf11fa3687706eddfa6a3f4ce6f
abdel2000-dply <abno870@gmail.com>
2023-07-11 23:35:09 +0100

    task 6

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,5 +4,5 @@ import json
 
 
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """returns an object represented by a JSON string"""
     return json.loads(my_str)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
     with open(filename, 'w') as file:
-        json.dumps(my_obj, file)
+        json.dump(my_obj, file)
