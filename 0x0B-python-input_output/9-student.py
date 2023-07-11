d03ebf8b53b09c3d8baa309a720f3d6567d31f1e
abdel2000-dply <abno870@gmail.com>
2023-07-12 00:24:49 +0100

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,9 +5,11 @@
 class Student:
     """Student class"""
     def __init__(self, first_name, last_name, age):
+        """init student"""
         self.first_name = first_name
 	self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """to json func"""
         return self.__dict__
