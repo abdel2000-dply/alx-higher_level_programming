9cee72ef1c22f3df9a77e4b24a80d3dd70aefa33
abdel2000-dply <abno870@gmail.com>
2023-07-12 00:37:25 +0100

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,5 +15,7 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs\
-                if hasattr(self, attr)}
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
