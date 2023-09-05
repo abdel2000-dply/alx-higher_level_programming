c14c6f9bc79830da1e5ebc7f21d0fa21dc2f45d1
abdel2000-dply <abno870@gmail.com>
2023-09-05 22:32:25 +0100

    everything is an object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(n=[]):
+    n.append("BestSchool")
+    return ", ".join(n)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is Locked class"""
+
+
+class LockedClass:
+    """Locked"""
+    __slots__ = ['first_name']
