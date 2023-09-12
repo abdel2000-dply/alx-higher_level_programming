81da0b5a8c2a1e3c6bec9999c1860bde77bc7453
abdel2000-dply <abno870@gmail.com>
2023-09-12 22:57:45 +0100

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+def pascal_triangle(n):
+    """pascal function"""
+
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                value = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(value)
+    triangle.append(row)
+
+    return triangle
