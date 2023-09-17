8a0574166d5fa19b68cc633bbd23fa280208916e
abdel2000-dply <abno870@gmail.com>
2023-09-17 19:34:07 +0100

    the first task

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Base Class """
+class Base:
+    """The goal of this class is to manage id attribute in all your future classes"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Iniyialize an instance """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
