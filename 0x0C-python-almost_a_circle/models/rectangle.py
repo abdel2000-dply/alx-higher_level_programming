2cc42d4c2963edc17f6a82e9f19dda8a43261a10
abdel2000-dply <abno870@gmail.com>
2023-09-19 20:52:33 +0100

    task 7: improving the display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,8 +70,10 @@ class Rectangle(Base):
 
     def display(self):
         """display of the rect with '#'"""
+        for j in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """the string representation of Rectangle"""
