d2cf9b018e65fdce8d163d95de05b2eb73122a33
abdel2000-dply <abno870@gmail.com>
2023-11-14 22:55:03 +0100

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
---lists all records of the table second_table
+-- lists all records of the table second_table
 
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name !='' ORDER BY score DESC;
