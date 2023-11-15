e5c98b22ea099a9c2d4e1e2061507c1463cd7f8e
abdel2000-dply <abno870@gmail.com>
2023-11-15 22:31:55 +0100

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 -- that have at least one genre linked.
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
 ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
