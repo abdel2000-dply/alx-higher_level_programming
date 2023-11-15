64f33734260b660b3f32ef83ba77a3ced9f30e4d
abdel2000-dply <abno870@gmail.com>
2023-11-15 22:33:27 +0100

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 INNER JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_show.title ASC, tv_show_genres.genre_id ASC;
