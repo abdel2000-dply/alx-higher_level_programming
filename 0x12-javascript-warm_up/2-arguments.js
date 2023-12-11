75e44063a1469792ecf5a2a14fb787612a37672d
abdel2000-dply <abno870@gmail.com>
2023-12-11 17:15:59 +0100

    task 2 :fire:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const n = process.argv.length;
 
 if (n <= 2) {
   console.log('No argument');
-} else if (n >= 3) {
+} else if (n === 3) {
+  console.log('Arguments found');
+} else {
   console.log('Arguments found');
 }
