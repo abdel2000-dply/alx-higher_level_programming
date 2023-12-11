d62f133a45d291936464d7035a3c2b80bdcfb6d0
abdel2000-dply <abno870@gmail.com>
2023-12-11 19:32:26 +0100

    task 7 :fire:

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const x = parseInt(process.argv[2]);
 
-if (isnan(x) || x < 0) {
+if (isNaN(x) || x < 0) {
   console.log('missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
