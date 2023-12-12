02a1ae41f7099720789ade63c709e2588bbf820f
abdel2000-dply <abno870@gmail.com>
2023-12-12 18:16:19 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,8 +7,8 @@ module.exports = class Square extends BaseSquare {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
       }
     }
   }
