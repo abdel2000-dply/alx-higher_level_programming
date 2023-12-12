3be7e75391e0989af509708816bd7f36490eab97
abdel2000-dply <abno870@gmail.com>
2023-12-12 19:19:33 +0100

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  for (let i = 0; i < list.length; i++) {
-    const t = list[list.length - i - 1];
-    list[list.length - i - 1] = list[i];
-    list[i] = t;
+  for (let i = 0; i < list.length / 2; i++) {
+    [list[i], list[list.length - i - 1]] = [list[list.length - i - 1], list[i]];
   }
   return list;
 };
