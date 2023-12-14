52534bb1e934918859909cef60785600cbf33572
abdel2000-dply <abno870@gmail.com>
2023-12-14 19:44:22 +0100

    task 1 :fire:

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""  lists all states with a name starting with N """
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    """ Connect to db """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    """ Create cursor """
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for state in cursor.fetchall():
+        print(state)
+
+    """ Close cursor and database connection """
+    cursor.close()
+    db.close()
