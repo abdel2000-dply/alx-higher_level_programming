8db2aec5ce9528ebb9dafbf917a84a3c8a285260
abdel2000-dply <abno870@gmail.com>
2023-12-15 21:46:59 +0100

    task 3 :fire:

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Return all table values where the name matches the argument - safe from MySQL injections! """
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    """ Connect to db """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    """ Create cursor """
+    cursor = db.cursor()
+    cursor.execute(
+        """SELECT *
+        FROM states WHERE name = %s ORDER BY id ASC""", (argv[4],)
+    )
+
+    for state in cursor.fetchall():
+            print(state)
+
+    """ Close cursor and database connection """
+    cursor.close()
+    db.close()
