d226df85be385371c00051e34bfd25011401dfcf
abdel2000-dply <abno870@gmail.com>
2023-12-15 18:59:49 +0100

    task 1 : update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,11 +8,13 @@ from sys import argv
 if __name__ == "__main__":
 
     """ Connect to db """
-    db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=argv[1],
-                        passwd=argv[2],
-                        db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
     """ Create cursor """
     cursor = db.cursor()
