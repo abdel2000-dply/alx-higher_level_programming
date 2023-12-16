da6e4259b6a5958b8ab02549dabf214bbb769267
abdel2000-dply <abno870@gmail.com>
2023-12-16 20:56:03 +0100

    task 7 :fire:

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-	states = session.query(State).order_by(State.id).all()
+	states = session.query(State).order_by(State.id)
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print("{:d}: {:s}".format(state.id, state.name))
 
     # Close the session
     session.close()
