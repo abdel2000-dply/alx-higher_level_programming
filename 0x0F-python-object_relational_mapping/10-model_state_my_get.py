13b5c81792ccee9fd2e3f87f38c372b35392e815
abdel2000-dply <abno870@gmail.com>
2023-12-16 22:21:56 +0100

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -2,13 +2,13 @@
 """
 Prints the State object with the name passed as argument
 """
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, db_name = argv[1], argv[2], argv[3]
 
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
