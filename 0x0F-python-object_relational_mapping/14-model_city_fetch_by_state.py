ca991e166c2728fc5c0dcf4b85fb3fa395b9287e
abdel2000-dply <abno870@gmail.com>
2023-12-16 23:35:21 +0100

    task 14, mandatory tasks DONE :fire: :fire:

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(State, City).join(
-        City, State.id == City.state_id).order_by(City.id)
+    cities = session.query(State, City).filter(
+        State.id == City.state_id).order_by(City.id)
     for s, c in cities:
-        print(f"{state.name}: ({city.id}) {city.name}")
+        print(f"{s.name}: ({c.id}) {c.name}")
 
     session.close()
