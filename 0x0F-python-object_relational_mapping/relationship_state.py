f20fa64583342a308c59473eed5b9b83dc421772
abdel2000-dply <abno870@gmail.com>
2023-12-17 19:35:01 +0100

    task 15

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -16,4 +16,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship('City', backref='state')
