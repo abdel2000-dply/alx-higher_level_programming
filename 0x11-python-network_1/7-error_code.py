1d2273194654a716619288df88f80fa05e5debd5
abdel2000-dply <abno870@gmail.com>
2024-01-06 15:32:26 -0800

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" Error code #1 """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    resp = requests.get(url)
+    if resp.status_code >= 400:
+        print("Error code:", resp.status_code)
+    else:
+        print(resp.text)
