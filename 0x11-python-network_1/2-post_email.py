935eeef8b8cbebf1d910e949fb76543297d40f0c
abdel2000-dply <abno870@gmail.com>
2024-01-06 13:51:16 -0800

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""  POST an emai """
+import urllib.request
+from sys import argv
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
