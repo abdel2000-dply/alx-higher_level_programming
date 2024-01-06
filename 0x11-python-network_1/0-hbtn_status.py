f06f6f33d648afdddfeed94f4251f7276a3d541c
abdel2000-dply <abno870@gmail.com>
2024-01-06 10:06:39 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         content = response.read()
