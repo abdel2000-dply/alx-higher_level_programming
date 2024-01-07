bda1f480a2b4f6a5af07311b2f4ab0bd39310b5e
abdel2000-dply <abno870@gmail.com>
2024-01-07 07:02:55 -0800

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Search API """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    q = argv[1] if len(argv) > 1 else ""
+    url = "http://0.0.0.0:5000/search_user" 
+    data = {'q': q}
+    response = requests.post(url, data)
+    try:
+        json_resp = response.json()
+        if json_resp:
+            print(f'[{json_resp.get("id")}] {json_resp.get("name")}')
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
+
