6ab291018f1a27517f94d700b58ac0ad5dca6caf
abdel2000-dply <abno870@gmail.com>
2024-01-07 08:52:39 -0800

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Time for an interview! """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    repo, owner = argv[1], argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    params = {"per_page": 10}
+
+    commits = requests.get(url, params=params).json()
+
+    for commit in commits:
+        name = commit.get("commit").get("author").get("name")
+        print(f'{commit.get("sha")}: {name}')
