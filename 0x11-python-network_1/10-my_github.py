a5c90e03df091d1fe7d29b57ca48f6d57b185fa0
abdel2000-dply <abno870@gmail.com>
2024-01-07 07:57:21 -0800

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" My GitHub! """
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username, passw = argv[1], argv[2]
+
+    response = requests.get(url, auth=(username, passw))
+    print(response.json().get("id"))
