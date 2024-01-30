92951a26e1d56787fbae487f8182535715ada035
abdel2000-dply <abno870@gmail.com>
2024-01-30 22:51:57 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request.get(url, (err, res, body) => {
+  if (err) console.error(err);
+  fs.writeFile(file, body, 'utf-8', (err) => {
+    if (err) console.error(err);
+  });
+});
