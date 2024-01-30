f701d199747209adaa6f8e0853119386af23f540
abdel2000-dply <abno870@gmail.com>
2024-01-31 00:17:56 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  const data = JSON.parse(body);
+  const res = {};
+  for (const task of data) {
+    if (task.completed) {
+      res[task.userId] = (res[task.userId] || 0) + 1;
+    }
+  }
+  console.log(res);
+});
