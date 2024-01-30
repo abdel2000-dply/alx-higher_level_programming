9915023655b0b83a06bd383182022026eb3091c0
abdel2000-dply <abno870@gmail.com>
2024-01-30 22:35:01 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const id = 18;
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    const films = data.results.filter((film) =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)
+    );
+    console.log(films.length);
+  }
+});
