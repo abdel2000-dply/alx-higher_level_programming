0e00a752277d810f1eee25316fdc1a9aa00d5a7b
abdel2000-dply <abno870@gmail.com>
2024-01-30 20:24:44 +0100

    task 3, Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
