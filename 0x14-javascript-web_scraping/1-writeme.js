95ed4275226f6f7adfc22d3346cca7f4d16f50cd
abdel2000-dply <abno870@gmail.com>
2024-01-30 20:00:15 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(file, text, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
