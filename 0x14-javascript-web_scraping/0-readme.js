177c0460a58c788704bb1e5891903c12d42464b2
abdel2000-dply <abno870@gmail.com>
2024-01-30 19:42:08 +0100

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', (err, data) => {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log(data);
+	}
+});
