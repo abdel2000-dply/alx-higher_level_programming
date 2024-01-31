e9671d6fa275208649ef09c03598b3a44143000c
abdel2000-dply <abno870@gmail.com>
2024-01-31 01:51:46 +0100

    task 8 :done:

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ const id = process.argv[2];
 const promisifiedRequest = util.promisify(request);
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request(url, async(err, response, body) => {
+request(url, async (err, response, body) => {
   if (err) console.log(err);
   const film = JSON.parse(body);
 
