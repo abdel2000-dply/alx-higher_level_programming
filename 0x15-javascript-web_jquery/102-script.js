92850a34e01d481cde2105819d509fd55978fc88
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-07 21:37:53 +0100

    task 12

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+/*
+  how to use the API provided by Stefan Bohacek:
+  https://stefanbohacek.com/project/hellosalut-api/
+*/
+const url = 'https://hellosalut.stefanbohacek.dev/';
+$(document).ready(() => {
+  $('INPUT#btn_translate').click(() => {
+    const language = $('INPUT#language_code').val();
+    $.get(url, { lang: language }, (data) => {
+      $('DIV#hello').text(data.hello);
+    });
+  });
+});
