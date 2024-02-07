8ee2af0dd10fe3281dd4a5a2ced40e4d465d6cc2
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-07 18:32:14 +0100

    task 8 : list all movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, function (data) {
+  for (const movie of data.results) {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  }
+});
