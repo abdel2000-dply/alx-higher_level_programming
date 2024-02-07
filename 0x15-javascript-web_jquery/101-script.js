2984b134bd867b56f59956c5a5daa6e64911f4fa
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-07 20:55:10 +0100

    task 11

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+$(document).ready(() => {
+  $('div#add_item').click(() => $('ul.my_list').append('<li>Item</li>'));
+  $('div#remove_item').click(() => $('ul.my_list li').last().remove());
+  $('div#clear_list').click(() => $('ul.my_list li').remove());
+});
